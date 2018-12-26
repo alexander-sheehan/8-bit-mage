1f5037b8251a1a82fe6b5a7ba72969bf9050056c
Alexander Sheehan <ajsheehan618@gmail.com>
2018-12-25 21:24:08 -0800

    added wall jumping, tightened up controls

--- 8BitMage/Assets/Scripts/characterMovement.cs
+++ 8BitMage/Assets/Scripts/characterMovement.cs
@@ -5,72 +5,79 @@ using System;
 
 public class characterMovement : MonoBehaviour {
 
-	public float maxSpeed = 15f;
-	public float maxSlideSpeed = 9f;
+	private float maxSpeed = 15f;
+	private float maxSlideSpeed = 3f;
 	public bool facingRight = true;
 	private Rigidbody2D rb2D;
+	private CircleCollider2D characterHitBox;
 	Animator anim;
 
 	public bool grounded = false;
-	float groundRadius = .2f;
+	float groundRadius = .3f;
 	public Transform groundCheck;
 	public LayerMask whatIsGround;
 
-	public float jumpForce = 700f;
+	public bool wallGrounded = false;
+	public Transform wallCheck;
+
+	private float jumpForce = 700f;
+
+	private float wallJumpYForce = 700;
+	private float wallJumpXForce = 1400f;
+
+	public bool leftWallTouch = false;
+	public bool rightWallTouch = false;
 
 	private void Awake()
 	{
 		// Get the RigidBody and Animator components of the Character so we can work with them
 		rb2D = GetComponent<Rigidbody2D>();
+		characterHitBox = GetComponent<CircleCollider2D> ();
 		anim = GetComponent<Animator> ();
 	}
 		
-	// Update is called once per frame
 	void FixedUpdate () 
 	{
-
-		/* WHAT I WANT TO DO GOING FORWARD:
-		 * if the player is inputing a horizontal movement (and player is not in the air)
-		 * play running animationo
-		 * else if the abs(player movement) is > 0
-		 * play sliding animation
-		 * else
-		 * play idle animation
-		 * 
-		 * How can this be improved?
-		 * 1: velocity change requirements to trigger slide animations (+ others?)
-		 * 		example: only slide when player is decreasing from a certain velocity
-		 */
-
-		/* TODO: 
-		* Create more of a slide on the character movment after input has ended
-		* Add wall sliding
-		* Fix animations in air
-		* Tighten controls
-		*/
 
 		// Initialized stuff for jumping and determining what is ground
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 		anim.SetBool ("Ground", grounded);
 		anim.SetFloat ("vSpeed", rb2D.velocity.y);
 
+		// wallGrounded is the boolean to determine whether we're touching a wall
+		// Touching a wall should allow the player to rejump, but numbers should be tweaked so that you cannot ascend a wall by only jumping
+		// I have no idea why it's the radius * 4.5 for the box size... it works, just accept it
+		wallGrounded = Physics2D.OverlapBox(wallCheck.position, new Vector2((characterHitBox.radius * 4.5f), .1f), 0f, whatIsGround);
+		anim.SetBool ("wallGrounded", wallGrounded);
+
+		// Left and right collider boxes that detect which wall the player is leaning against
+		// Once again.. no idea why I have to multiply the radius by 2.5... you can fix it if you want GL HF
+		Vector2 leftWallHitBoxPos = new Vector2 (wallCheck.position.x - characterHitBox.radius, wallCheck.position.y);
+		Vector2 leftWallHitBoxSize = new Vector2 (characterHitBox.radius * 2.5f, .1f);
+		leftWallTouch = Physics2D.OverlapBox (leftWallHitBoxPos, leftWallHitBoxSize, 0f, whatIsGround);
+
+		Vector2 rightWallHitBoxPos = new Vector2 (wallCheck.position.x + characterHitBox.radius, wallCheck.position.y);
+		Vector2 rightWallHitBoxSize = new Vector2 (characterHitBox.radius * 2.5f, .1f);
+		rightWallTouch = Physics2D.OverlapBox (rightWallHitBoxPos, rightWallHitBoxSize, 0f, whatIsGround);
+
 		// Get the input of the player (on the x axis)
 		float move = Input.GetAxisRaw ("Horizontal");
 			
 		// First attempt at getting a slide animation into the movement cycle
-		// Sliding should only happen when the player releases inputs and 
-		if (Mathf.Abs (move) > 0) 
+		// Sliding should only happen when the player releases inputs while running at a certain top speed
+		if (Mathf.Abs (move) > 0)
 			anim.SetInteger ("movementState", 1);
-		else if (Mathf.Abs(rb2D.velocity.x) > maxSlideSpeed && !Input.GetButton ("Horizontal"))
+		else if (Mathf.Abs (rb2D.velocity.x) > maxSlideSpeed && !Input.GetButton ("Horizontal"))
 			anim.SetInteger ("movementState", 2);
 		else 
 			anim.SetInteger ("movementState", 0);
 
 
 		// This section deals with horizontal acceleration and deceleration
+		// It's linear... bite me t(-,-t)
 		float newSpeed = rb2D.velocity.x;
 
-		// With linear accerlation, I don't think this *kick off* part is necessary
+		// With linear acceleration, I don't think this *kick off* part is necessary
 		// But fuck it, it's here and I'm too lazy to change it
 		if (newSpeed == 0) {
 			if (move > 0) {
@@ -79,17 +86,22 @@ public class characterMovement : MonoBehaviour {
 				newSpeed = -1f;
 			}
 
-		// We're linearlly accelerating the player, so adding flat amounts based on input and current velocity
+		// We're adding flat amounts to the players velocity every frame based on user input
 		} else if (move > 0) {
-			newSpeed += 1.6f;
+			newSpeed += 1.2f;
+
 		} else if (move < 0) {
-			newSpeed -= 1.6f;
+			newSpeed -= 1.2f;
+		
+			// If we're close to 0, just make it 0 (I don't know why I'm doing this, bite me again t(-,-t) )
 		} else if (newSpeed > -.9f && newSpeed < .9f){
 			newSpeed = 0;
+
 		} else if (move == 0 && newSpeed > 0) {
-			newSpeed -= .1f;
+			newSpeed -= .2f;
+
 		} else if (move == 0 && newSpeed < 0) {
-			newSpeed += .1f;
+			newSpeed += .2f;
 		}
 
 		// Capping speeds based on global `maxSpeed`
@@ -107,17 +119,33 @@ public class characterMovement : MonoBehaviour {
 			Flip ();
 		else if (move < 0 && facingRight)
 			Flip ();
+		else if (!facingRight && rightWallTouch)
+			Flip ();
+		else if (facingRight && leftWallTouch)
+			Flip ();
 	}
 
 	void Update()
 	{
 		// Because jumping is a single button instance (you click it and don't hold it down),
 		// we want this to live in Update and not FixedUpdate
-		// If we're on the ground and the player presses `space`
 		// TODO: 
-		// Wall jumping 
 		// Holding down space increases the height of a jump?
-		if (grounded && Input.GetKeyDown (KeyCode.Space)){
+		if (wallGrounded && !grounded && Input.GetKeyDown (KeyCode.Space)) {
+
+			// This is for animations. When the player jumps- make them not grounded
+			anim.SetBool ("Ground", false);
+
+			//For wall jumping
+			if (facingRight) 
+
+				// When on a wall, add force to both x and y axis'
+				rb2D.AddForce (new Vector2 (-wallJumpXForce, wallJumpYForce));
+			else 
+				rb2D.AddForce (new Vector2 (wallJumpXForce, wallJumpYForce));
+
+		// If we're not on a wall, then jump normal
+		} else if (grounded && Input.GetKeyDown (KeyCode.Space)){
 
 			// This is for animations
 			anim.SetBool ("Ground", false);
