61a9f61a30c69592d403270004230838f78bee52
Alexander Sheehan <ajsheehan618@gmail.com>
2019-01-16 00:19:13 -0500

    This shit sucks.. but it kinda tworks so I'm calling it :^)

---
+++ 8BitMage/Assets/Scripts/buttonController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonController : MonoBehaviour {
+	public GameObject panel;
+
+	public void spellOnclick() {
+		panel.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 8BitMage/Assets/Scripts/skeleSpellController.cs
+++ 8BitMage/Assets/Scripts/skeleSpellController.cs
@@ -14,6 +14,7 @@ public class skeleSpellController : MonoBehaviour {
 	public Animator anim;
 	private GameObject instantiatedObj;
 
+	public GameObject panel;
 	public Text question;
 	public Image iconImage;
 	public Button close;
@@ -23,19 +24,14 @@ public class skeleSpellController : MonoBehaviour {
 	public Button earth;
 	public Button electricity;
 
-//	// Use this for initialization
-//	void Start () {
-//		
-//	}
-	
-	// Update is called once per frame
+
+	// This can be improved.... a lot
 	void Update () {
 		if (Vector3.Distance (transform.position, character.transform.position) < 3f) {
 			if (!exists) {
 				Vector3 alertPosition = new Vector3 (transform.position.x, transform.position.y + 1.5f, 0f);
 				instantiatedObj = (GameObject) Instantiate (alert, alertPosition, Quaternion.identity);
 				exists = true;
-
 			}
 		} else {
 			exists = false;
@@ -46,9 +42,16 @@ public class skeleSpellController : MonoBehaviour {
 
 		if (exists) {
 			if (Input.GetKeyDown(KeyCode.Return)) {
-				
+				if (!panel.activeSelf) {
+					panel.SetActive (true);
+				}
 			}
 		}
-
+		if (panel.activeSelf) {
+			if (Input.GetKeyDown(KeyCode.Escape) || Vector3.Distance (transform.position, character.transform.position) > 3f){
+				panel.SetActive (false);
+			}
+		}
 	}
+
 }
